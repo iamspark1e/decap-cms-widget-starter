50cbe173f79a036972f225e7f465673112e0209a
iamspark1e <wenbo@sparkle.im>
2023-08-24 11:23:27 +0800

    feat: use conditional import for dev mode

---
+++ src/dev.js
@@ -0,0 +1,4 @@
+// Codes below will not be included when running in "production" mode.
+window.CMS_MANUAL_INIT = true;
+CMS.registerWidget('test', window.StarterControl, window.StarterPreview);
+CMS.init();
